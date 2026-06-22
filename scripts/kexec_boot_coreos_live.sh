f3bfee5e3075757b82c93048c16cfc9f3fca1288
Kamil <Kamildudek2@gmail.com>
2026-06-22 12:16:21 +0200

    fix script

--- scripts/kexec_boot_coreos_live.sh
+++ scripts/kexec_boot_coreos_live.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# 1. Get current stable version
-VER=$(curl -s https://builds.coreos.fedoraproject.org/streams/stable.json \
-  | python3 -c "import sys,json; d=json.load(sys.stdin); \
-    print(d['architectures']['x86_64']['artifacts']['metal']['release'])")
 
-BASE="https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/${VER}/x86_64"
+# Get URLs from JSON
+read KERNEL INITRAMFS ROOTFS < <(curl -s https://builds.coreos.fedoraproject.org/streams/stable.json | python3 -c "
+import sys, json
+d = json.load(sys.stdin)
+pxe = d['architectures']['x86_64']['artifacts']['metal']['formats']['pxe']
+print(pxe['kernel']['location'], pxe['initramfs']['location'], pxe['rootfs']['location'])
+")
 
-# 2. Download all three artifacts
-curl -O "${BASE}/fedora-coreos-${VER}-live-kernel-x86_64"
-curl -O "${BASE}/fedora-coreos-${VER}-live-initramfs.x86_64.img"
-curl -O "${BASE}/fedora-coreos-${VER}-live-rootfs.x86_64.img"
+# Download
+curl -O "$KERNEL"
+curl -O "$INITRAMFS"
+curl -O "$ROOTFS"
 
-# 3. Combine initramfs + rootfs
-cat fedora-coreos-${VER}-live-initramfs.x86_64.img \
-    fedora-coreos-${VER}-live-rootfs.x86_64.img \
+# Combine
+cat fedora-coreos-*-live-initramfs.x86_64.img \
+    fedora-coreos-*-live-rootfs.x86_64.img \
     > combined.img
 
-# 4. Load and execute
-kexec -l fedora-coreos-${VER}-live-kernel-x86_64 \
-  --initrd=combined.img \
-  --append="ip=dhcp rd.neednet=1 ignition.platform.id=metal"
+# kexec
+# kexec -l fedora-coreos-*-live-kernel.x86_64 \
+#   --initrd=combined.img \
+#   --append="ip=dhcp rd.neednet=1 ignition.platform.id=metal \
+#     ignition.config.data=$(base64 -w0 live.ign)"
 
-sync && kexec -e
+# sync && kexec -e
